0e597d0a7275db890c6a4330f3421a1a7aea42e1
David Ranieri <davranfor@gmail.com>
2023-05-16 08:15:24 +0200

    Cleaned test callback_filter

--- test/callback_filter/demo.c
+++ test/callback_filter/demo.c
@@ -7,10 +7,10 @@
 #include <locale.h>
 #include <json/json.h>
 
-/* Return 0 to continue. !0 to stop traversing */
+/* Callback function - Return 0 to continue or not 0 to stop traversing */
 static int sum_numbers_until_null(const json *node, int depth, void *data)
 {
-    (void)depth; // We don't need the depth of the node in this example
+    (void)depth;
     if (json_is_number(node)) // Returns true for integer, real and double
     {
         *(double *)data += json_number(node);
@@ -23,20 +23,21 @@ int main(void)
 {
     setlocale(LC_CTYPE, "");
 
-    json *root, *tail = NULL;
+    json *root, *tail;
     double sum = 0.0;
 
     root = json_new_array(NULL);
-    tail = json_push_fast(root, tail, json_new_string(NULL, "foo"));
-    tail = json_push_fast(root, tail, json_new_integer(NULL, -39));
-    tail = json_push_fast(root, tail, json_new_real(NULL, 42));
-    tail = json_push_fast(root, tail, json_new_boolean(NULL, 1)); // Not computed
-    tail = json_push_fast(root, tail, json_new_string(NULL, "bar"));
-    tail = json_push_fast(root, tail, json_new_object(NULL));
-    tail = json_push_fast(root, tail, json_new_double(NULL, 0.14, 2)); // 2=decimals
-    tail = json_push_fast(root, tail, json_new_null(NULL)); // Stop here
-    tail = json_push_fast(root, tail, json_new_integer(NULL, 100));
-    tail = json_push_fast(root, tail, json_new_string(NULL, "baz"));
+    tail = json_push_front(root, json_new_string(NULL, "foo"));
+    tail = json_push_after(tail, json_new_integer(NULL, -39));
+    tail = json_push_after(tail, json_new_real(NULL, 42));
+    tail = json_push_after(tail, json_new_boolean(NULL, 1)); // Not computed
+    tail = json_push_after(tail, json_new_string(NULL, "bar"));
+    tail = json_push_after(tail, json_new_object(NULL));
+    tail = json_push_after(tail, json_new_string(NULL, "100")); // Not computed
+    tail = json_push_after(tail, json_new_double(NULL, 0.14, 2)); // 2 decimals
+    tail = json_push_after(tail, json_new_null(NULL)); // Stop here
+    tail = json_push_after(tail, json_new_integer(NULL, 100));
+    tail = json_push_after(tail, json_new_string(NULL, "baz"));
     json_traverse(root, sum_numbers_until_null, &sum);
     printf("Sum = %.2f\n", sum); // should be 3.14
     json_free(root);

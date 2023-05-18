82d6ee6b1637e5eee0dfa90088f44268f1fd23cb
David Ranieri <davranfor@gmail.com>
2023-05-18 16:13:33 +0200

    Added json_sort test example

---
+++ test/sort/demo.c
@@ -0,0 +1,43 @@
+/*!
+ *  \brief     strictissimo - Ligtweight json and json-schema library for C
+ *  \author    David Ranieri <davranfor@gmail.com>
+ *  \copyright GNU Public License.
+ */
+
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include <json/json.h>
+
+static int compare(const json *a, const json *b)
+{
+    if (json_is_scalar(a) && json_is_scalar(b))
+    {
+        return json_number(a) > json_number(b);
+    }
+    return json_type(a) > json_type(b);
+}
+
+int main(void)
+{
+    setlocale(LC_CTYPE, "");
+    srand((unsigned)time(NULL));
+
+    json *root, *tail = NULL;
+
+    root = json_new_array(NULL);
+    for (int i = 0; i < 25; i++)
+    {
+        int r = rand() % 100;
+
+        tail = json_push_fast(root, tail, json_new_integer(NULL, r));
+    }
+    puts("Unsorted:");
+    json_print(root);
+    json_sort(root, compare);
+    puts("Sorted:");
+    json_print(root);
+    json_free(root);
+    return 0;
+}
+

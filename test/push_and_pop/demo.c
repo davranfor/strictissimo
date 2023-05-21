1b4f0cb8cf977e9149c903ce01652b51fd9404f8
David Ranieri <davranfor@gmail.com>
2023-05-21 12:11:52 +0200

    Cleaning push_and_pop test example

--- test/push_and_pop/demo.c
+++ test/push_and_pop/demo.c
@@ -15,64 +15,86 @@
 #include <time.h>
 #include <json/json.h>
 
-enum {N = 50};
+#define none (-1u)
+
+static void print(const char *function, const json *node,
+    unsigned offset, unsigned number)
+{
+    printf("{\"function\": \"%s\"", function);
+    if (offset != none)
+    {
+        printf(", \"offset\": %u", offset);
+    }
+    if (number != none)
+    {
+        printf(", \"number\": %u", number);
+    }
+    printf(", \"result\": %s}\n", node != NULL ? "true" : "false");
+}
 
 int main(void)
 {
+    enum {N = 50};
+
     srand((unsigned)time(NULL));
 
-    json *root, *node;
+    json *root = json_new_array("array");
 
-    root = json_new_array("array");
     for (size_t i = 0; i < N; i++)
     {
         unsigned number = (unsigned)rand() % 90 + 10;
         unsigned offset = (unsigned)rand() % 5;
         unsigned random = (unsigned)rand() % 9;
+        json *node;
 
         switch (random)
         {
             case 0:
-                printf("json_push_front(%u)\n", number);
-                json_push_front(root, json_new_real(NULL, number));
+                node = json_push_front(root, json_new_real(NULL, number));
+                print("json_push_front", node, none, number);
                 break;
             case 1:
-                printf("json_push_back(%u)\n", number);
-                json_push_back(root, json_new_real(NULL, number));
+                node = json_push_back(root, json_new_real(NULL, number));
+                print("json_push_back", node, none, number);
                 break;
             case 2:
-                printf("json_push_before(#%u: %u)\n", offset, number);
                 if ((node = json_at(root, offset)))
                 {
                     json_push_before(node, json_new_real(NULL, number));
                 }
+                print("json_push_before", node, offset, number);
                 break;
             case 3:
-                printf("json_push_after(#%u: %u)\n", offset, number);
                 if ((node = json_at(root, offset)))
                 {
                     json_push_after(node, json_new_real(NULL, number));
                 }
+                print("json_push_after", node, offset, number);
                 break;
             case 4:
-                printf("json_push_at(#%u: %u)\n", offset, number);
-                json_push_at(root, json_new_real(NULL, number), offset);
+                /* If offset is out of bounds it acts like json_push_back() */
+                node = json_push_at(root, json_new_real(NULL, number), offset);
+                print("json_push_at", node, offset, number);
                 break;
             case 5:
-                printf("json_pop(#%u)\n", offset);
-                json_free(json_pop(json_at(root, offset)));
+                node = json_pop(json_at(root, offset));
+                print("json_pop", node, offset, none);
+                json_free(node);
                 break;
             case 6:
-                printf("json_pop_front()\n");
-                json_free(json_pop_front(root));
+                node = json_pop_front(root);
+                print("json_pop_front", node, none, none);
+                json_free(node);
                 break;
             case 7:
-                printf("json_pop_back()\n");
-                json_free(json_pop_back(root));
+                node = json_pop_back(root);
+                print("json_pop_back", node, none, none);
+                json_free(node);
                 break;
             case 8:
-                printf("json_pop_at(#%u)\n", offset);
-                json_free(json_pop_at(root, offset));
+                node = json_pop_at(root, offset);
+                print("json_pop_at", node, offset, none);
+                json_free(node);
                 break;
             default:
                 break;
